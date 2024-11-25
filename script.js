24a3d6586c5bac5a3ac2555e15a73331d7d5ca8e
abhishekkushwah777 <abhishekkushwah1359@gmail.com>
2024-11-25 07:44:18 -0800

    added some pages & a menu sidebar

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function togglenavmenu() {
     const menu = document.getElementById("nav-menu");
-    menu.classList.toggle("hidden");
+    menu.classList.toggle("visible");
+
+    if (menu.classList.contains("visible")){
+        menu.classList.remove("hidden");
+    } else{
+        menu.classList.add("hidden");
+    }
 };
